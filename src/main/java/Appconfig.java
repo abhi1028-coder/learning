50e956e87c8bd08ad137ebc396da0f2622c47640
abhi1028-coder <64057583+abhi1028-coder@users.noreply.github.com>
2021-10-19 14:55:24 -0500

    this is new file

---
+++ src/main/java/Appconfig.java
@@ -0,0 +1,11 @@
+
+public class Appconfig {
+
+    public static void main(String[] args) {
+        System.out.println("This is Main Method");
+
+        Sample smp = new Sample();
+        smp.sample2(10,20,30);
+
+    }
+}
