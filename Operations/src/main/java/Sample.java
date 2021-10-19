88f8fc5abb9ee12510eb001690704c27c124a3b8
abhi1028-coder <64057583+abhi1028-coder@users.noreply.github.com>
2021-10-19 14:35:38 -0500

    this is new file

---
+++ Operations/src/main/java/Sample.java
@@ -0,0 +1,13 @@
+public class Sample {
+    void sample2(int a, int b, int c){
+        int sum = a+b;
+        int Mul= a- b;
+        int div= a/b;
+
+        System.out.println("addition, Subtraction and Multiplication of a & b " + sum);
+        System.out.println("addition, Subtraction and Multiplication of a & b " + Mul);
+        System.out.println("addition, Subtraction and Multiplication of a & b " + div);
+
+    }
+
+}
